fe379a5f57adf101d837fcb4f464de6b5c9c9c2d
olga <koa0308@gmail.com>
2026-03-27 22:10:54 +0400

    корректировка

--- config/settings.py
+++ config/settings.py
@@ -92,8 +92,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": 'habit_tracker',
         "USER": 'postgres',
-        "PASSWORD": os.getenv("password"),
-        "HOST": os.getenv("host"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": "5432",
         "OPTIONS": {"client_encoding": "UTF8"},
     }
